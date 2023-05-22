250660daf587c7d4293c371a96ec2d6b038a64c0
VictorOrrillo <victor.orillo@tecsup.edu.pe>
2023-05-21 20:28:54 -0500

    Agregar archivos de proyecto.

---
+++ Semana07/Semana07/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Semana07.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Semana07
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSpeak_Clicked(object sender, EventArgs e)
+        {
+
+            DependencyService.Get<ITextToSpeech>().Speak(txtMessage.Text);
+
+        }
+    }
+}
